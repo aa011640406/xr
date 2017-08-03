05c7df40dfdfc19a37e043c6757f2349af51fc14
stone <2191445306@qq.com>
2017-08-03 14:27:34 +0800

    [release:11]
    Signed-off-by: stone <2191445306@qq.com>

---
+++ mycart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.1.140:8089/admin/');
+define('HTTP_CATALOG', 'http://192.168.1.140:8089/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.1.140:8089/admin/');
+define('HTTPS_CATALOG', 'http://192.168.1.140:8089/');
+
+// DIR
+define('DIR_APPLICATION', '/www/sites/mycart/admin/');
+define('DIR_SYSTEM', '/www/sites/mycart/system/');
+define('DIR_IMAGE', '/www/sites/mycart/image/');
+define('DIR_LANGUAGE', '/www/sites/mycart/admin/language/');
+define('DIR_TEMPLATE', '/www/sites/mycart/admin/view/template/');
+define('DIR_CONFIG', '/www/sites/mycart/system/config/');
+define('DIR_CACHE', '/www/sites/mycart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/www/sites/mycart/system/storage/download/');
+define('DIR_LOGS', '/www/sites/mycart/system/storage/logs/');
+define('DIR_MODIFICATION', '/www/sites/mycart/system/storage/modification/');
+define('DIR_UPLOAD', '/www/sites/mycart/system/storage/upload/');
+define('DIR_CATALOG', '/www/sites/mycart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '111111');
+define('DB_DATABASE', 'mycart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
---
+++ mycart/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.1.140:8089/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.1.140:8089/');
+
+// DIR
+define('DIR_APPLICATION', '/www/sites/mycart/catalog/');
+define('DIR_SYSTEM', '/www/sites/mycart/system/');
+define('DIR_IMAGE', '/www/sites/mycart/image/');
+define('DIR_LANGUAGE', '/www/sites/mycart/catalog/language/');
+define('DIR_TEMPLATE', '/www/sites/mycart/catalog/view/theme/');
+define('DIR_CONFIG', '/www/sites/mycart/system/config/');
+define('DIR_CACHE', '/www/sites/mycart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/www/sites/mycart/system/storage/download/');
+define('DIR_LOGS', '/www/sites/mycart/system/storage/logs/');
+define('DIR_MODIFICATION', '/www/sites/mycart/system/storage/modification/');
+define('DIR_UPLOAD', '/www/sites/mycart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '111111');
+define('DB_DATABASE', 'mycart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
